c19b1993da962af03ac6d6f4dc98d55a7acbc656
Matheus Ferreira De Araujo <matheusferreiradearaujo19@gmail.com>
2023-03-09 15:24:00 -0300

    ola

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from conexao import criar_conexao, fechar_conexao
 
-def insere_usuario(con, nome, email, senha):
+def insere_usuarios(con, nome, email, senha):
     cursor = con.cursor()
-    sql = "INSERT INTO usuario(nome, email, senha) values(%s, %s, %s)"
+    sql = "INSERT INTO usuarios(nome, email, senha) values(%s, %s, %s)"
     valores = (nome, email, senha)
     cursor.execute(sql, valores)
     cursor.close()
@@ -11,7 +11,7 @@ def insere_usuario(con, nome, email, senha):
 def main():
     con =  criar_conexao("127.0.0.1", "root", "", "crip")
 
-    insere_usuario(con, "matheus", "matheus@araujo.com.br", "matheus123 ")
+    insere_usuarios(con, "matheus", "matheus@araujo.com.br", "matheus123 ")
 
     fechar_conexao(con)
 
